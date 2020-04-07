d888991562f80f4821e49b082a70a27a0cae5fe2
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-07 11:12:40 +0900

    Update the example about http4s client

--- http4s-example/src/main/scala/Client.scala
+++ http4s-example/src/main/scala/Client.scala
@@ -1,4 +1,6 @@
 import cats.effect._
+import cats.implicits._
+import org.http4s.Uri
 import org.http4s.client.blaze._
 import org.http4s.client._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -8,10 +10,18 @@ object Client extends App {
   implicit val cs: ContextShift[IO] = IO.contextShift(global)
   implicit val timer: Timer[IO] = IO.timer(global)
 
-  BlazeClientBuilder[IO](global).resource.use { client =>
-    // use `client` here and return an `IO`.
-    // the client will be acquired and shut down
-    // automatically each time the `IO` is run.
-    IO.unit
+  val greetingList = BlazeClientBuilder[IO](global).resource.use { client =>
+    def hello(name: String): IO[String] = {
+      val target = Uri.uri("http://localhost:8081/hello/") / name
+      client.expect[String](target)
+    }
+
+    val people = Vector("Michael", "Jessica", "Ashley", "Christopher")
+
+    people.parTraverse(hello)
   }
+
+  val greetingsStringEffect = greetingList.map(_.mkString("\n"))
+
+  println(greetingsStringEffect.unsafeRunSync)
 }
