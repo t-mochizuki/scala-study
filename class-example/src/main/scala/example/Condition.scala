af9fd1378a458296c639d0ad228ee636cc192808
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-04-09 19:02:54 +0900

    Update the example

--- class-example/src/main/scala/example/Condition.scala
+++ class-example/src/main/scala/example/Condition.scala
@@ -3,3 +3,7 @@ package example
 sealed trait ConditionT
 case class SearchCondition(key: String, op: WhereOperator, value: String) extends ConditionT
 case class Condition(condition: Seq[ConditionT]) extends ConditionT
+
+sealed abstract class Operator(value: String) extends ConditionT
+case object And extends Operator("And")
+case object Or extends Operator("Or")
--- class-example/src/main/scala/example/Main.scala
+++ class-example/src/main/scala/example/Main.scala
@@ -2,7 +2,11 @@ package example
 
 object Main extends App {
   val condition =
-    Condition(Seq(SearchCondition("name", Equals, "nana")))
+    Condition(
+      Seq(
+        SearchCondition("name", Equals, "nana"),
+        Or,
+        SearchCondition("name", Equals, "taro")))
 
   println(condition)
 }
