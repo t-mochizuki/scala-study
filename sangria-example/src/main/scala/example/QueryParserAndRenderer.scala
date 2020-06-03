4eb6a73bfcef48b8b5094242758bce8a5eb5505c
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-03 11:50:25 +0900

    Add an example in Query Parser and Renderer
    cf. https://sangria-graphql.org/learn/#query-parser-and-renderer

---
+++ sangria-example/src/main/scala/example/QueryParserAndRenderer.scala
@@ -0,0 +1,44 @@
+package example
+
+import sangria.ast.Document
+import sangria.parser.QueryParser
+import sangria.renderer.QueryRenderer
+
+import scala.util.{Failure, Success}
+
+object QueryParserAndRenderer extends App {
+  val query =
+    """
+      query FetchLukeAndLeiaAliased(
+            $someVar: Int = 1.23
+            $anotherVar: Int = 123) @include(if: true) {
+        luke: human(id: "1000")@include(if: true){
+          friends(sort: NAME)
+        }
+
+        leia: human(id: "10103\n \u00F6 ö") {
+          name
+        }
+
+        ... on User {
+          birth{day}
+        }
+
+        ...Foo
+      }
+
+      fragment Foo on User @foo(bar: 1) {
+        baz
+      }
+    """
+
+  // Parse GraphQL query
+  QueryParser.parse(query) match {
+    case Success(document) =>
+      // Pretty rendering of the GraphQL query as a `String`
+      println(document.renderPretty)
+
+    case Failure(error) =>
+      println(s"Syntax error: ${error.getMessage}")
+  }
+}
