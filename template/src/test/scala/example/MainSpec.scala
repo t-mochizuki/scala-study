2712ad063639a78f8e4b716e628f4da5397b4a61
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-08 14:00:44 +0900

    Fix a bit

--- template/src/test/scala/example/MainSpec.scala
+++ template/src/test/scala/example/MainSpec.scala
@@ -8,6 +8,6 @@ class MainSpec extends FlatSpec with DiagrammedAssertions {
   }
 
   "hello" should "return \"Hello, world.\"" in {
-    assert(Main.hello === "Hello, world!")
+    assert(Main.hello === "Hello, world.")
   }
 }
