c97ceba4d3d3c9a819be42300c0b4582b8a97797
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-09 18:21:01 +0900

    Fix a bit

--- template/src/test/scala/example/MainSpec.scala
+++ template/src/test/scala/example/MainSpec.scala
@@ -3,10 +3,6 @@ package example
 import org.scalatest.{DiagrammedAssertions, FlatSpec}
 
 class MainSpec extends FlatSpec with DiagrammedAssertions {
-  "main" should "be succeeded" in {
-    Main.main(Array("http://akka.io"))
-  }
-
   "hello" should "return \"Hello, world.\"" in {
     assert(Main.hello === "Hello, world.")
   }
