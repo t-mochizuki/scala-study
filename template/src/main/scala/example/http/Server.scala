2452fa669de20625366a76e864b6ee978cbe44f7
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-10 21:20:45 +0900

    Remove unused import

--- template/src/main/scala/example/http/Server.scala
+++ template/src/main/scala/example/http/Server.scala
@@ -1,16 +1,11 @@
 package example.http
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ ContentTypes, HttpEntity }
-import akka.http.scaladsl.server.HttpApp
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
-import scala.io.StdIn
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.{HttpApp, Route}
 
 class Server extends HttpApp {
 
-  val routes =
+  override def routes: Route =
     path("hello") {
       get {
         complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
