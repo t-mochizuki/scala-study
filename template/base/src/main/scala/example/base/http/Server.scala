8ed366052ef2a098a774001873852c5e9f7a5bda
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-13 16:50:16 +0900

    Add an exercise

--- template/base/src/main/scala/example/base/http/Server.scala
+++ template/base/src/main/scala/example/base/http/Server.scala
@@ -1,11 +1,17 @@
 package example.base.http
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.{HttpApp, Route}
-import example.base.dao.PersonEntity
 import java.time.{ZoneId, ZonedDateTime}
 
-class Server extends HttpApp {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.headers.Accept
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest, MediaTypes, Uri}
+import akka.http.scaladsl.server.{HttpApp, Route}
+import example.base.dao.PersonEntity
+import example.base.http.Client
+import example.base.logging.Logger
+
+class Server extends HttpApp with Logger {
 
   val zonedDateTime = ZonedDateTime.of(2020, 6, 8, 0, 0, 0, 0, ZoneId.systemDefault)
   val mother = PersonEntity("keiko", 23, zonedDateTime)
@@ -17,6 +23,19 @@ class Server extends HttpApp {
         complete(
           HttpEntity(ContentTypes.`application/json`, input)
         )
+      }
+    } ~
+    path("foo") {
+      get {
+        extractActorSystem { implicit system =>
+          extractExecutionContext { implicit ec =>
+            val uri = "http://localhost:8181/hello"
+            val acceptHeader: HttpHeader = Accept(MediaTypes.`application/json`)
+            val headers: Seq[HttpHeader] = Seq(acceptHeader)
+            val httpRequest = HttpRequest(method = HttpMethods.GET, uri = Uri(uri), headers = headers)
+            complete(Client.send(httpRequest))
+          }
+        }
       }
     }
 
