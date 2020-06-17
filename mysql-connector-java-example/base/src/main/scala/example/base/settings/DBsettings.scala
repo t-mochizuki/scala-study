14b2270c35381993735208d838d66956886c69c5
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-17 17:26:29 +0900

    Semicolon is not required in a query for MySQL
    I made a mistake.

--- mysql-connector-java-example/base/src/main/scala/example/base/settings/DBsettings.scala
+++ mysql-connector-java-example/base/src/main/scala/example/base/settings/DBsettings.scala
@@ -8,26 +8,14 @@ trait DBSettings {
 
 object DBSettings {
 
-  @SuppressWarnings(Array(
-    "org.wartremover.warts.NonUnitStatements",
-    "org.wartremover.warts.Return"))
+  @SuppressWarnings(
+    Array("org.wartremover.warts.NonUnitStatements", "org.wartremover.warts.Return")
+  )
   def initialize(): Unit =
     this.synchronized {
       if (ConnectionPool.isInitialized(Symbol("default"))) return
       DBs.loadGlobalSettings()
-      val JDBCSettings(url, user, password, _) = DBs.readJDBCSettings(Symbol("default"))
-      val configMap = DBs.readAsMap(Symbol("default"))
-      val settings = ConnectionPoolSettings(
-        initialSize = configMap.get("poolInitialSize").map(_.toInt).getOrElse(0),
-        maxSize = configMap.get("poolMaxSize").map(_.toInt).getOrElse(8),
-        connectionTimeoutMillis = configMap.get("poolConnectionTimeoutMillis").map(_.toLong).getOrElse(5000L),
-        validationQuery = "select 1;")
-      ConnectionPool.singleton(
-        url,
-        user,
-        password,
-        settings
-      )
+      DBs.setup(Symbol("default"))
     }
 
 }
