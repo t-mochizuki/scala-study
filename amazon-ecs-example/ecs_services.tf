9bda1245934322e73b3f2b4a80c0e4cd347404e9
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-20 21:12:09 +0900

    PragmaticTerraformOnAWS 9.2.3 ECS サービス

---
+++ amazon-ecs-example/ecs_services.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_service" "example" {
+  name                              = "example"
+  cluster                           = aws_ecs_cluster.example.arn
+  task_definition                   = aws_ecs_task_definition.example.arn
+  desired_count                     = 2
+  launch_type                       = "FARGATE"
+  platform_version                  = "1.4.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [aws_security_group.example.id]
+    subnets          = [aws_subnet.private.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
