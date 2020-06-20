200d22daba1cb32e5696d43c0e7d9dd62e345f6e
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-20 16:40:46 +0900

    プライベートネットワークからインターネットへ通信するためにルートの追加

--- amazon-ecs-example/routes.tf
+++ amazon-ecs-example/routes.tf
@@ -3,3 +3,9 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
