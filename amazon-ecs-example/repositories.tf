7e439d432e670550d4084411381f06b68bfeb382
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-20 22:10:42 +0900

    PragmaticTerraformOnAWS 14.2.e ECR ライフサイクルポリシー

--- amazon-ecs-example/repositories.tf
+++ amazon-ecs-example/repositories.tf
@@ -1,3 +1,27 @@
 resource "aws_ecr_repository" "example" {
   name = "example"
 }
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.example.name
+
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 10 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 10
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+EOF
+}
