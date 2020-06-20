d79c6df20f44d66889681f762e5998d91b5fa3ee
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-20 17:47:20 +0900

    PragmaticTerraformOnAWS 8.6.2 リスナールール

---
+++ amazon-ecs-example/listener_rules.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
