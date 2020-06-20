3781671a0c0add8ccb71937b4bb5c5214af95d28
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-20 16:25:46 +0900

    PragmaticTerraformOnAWS 7.2.1 プライベートサブネット

--- amazon-ecs-example/subnets.tf
+++ amazon-ecs-example/subnets.tf
@@ -4,3 +4,10 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone       = "ap-northeast-1a"
 }
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.64.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "ap-northeast-1a"
+}
