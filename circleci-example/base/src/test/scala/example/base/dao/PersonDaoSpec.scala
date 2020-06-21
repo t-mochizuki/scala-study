d5a963d57ee881a45ac2a4ba95bbcd45637e597c
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-21 18:18:56 +0900

    Add .circleci/config.yml
    cf. https://support.circleci.com/hc/en-us/articles/115015426888-Clear-project-dependency-cache

---
+++ circleci-example/base/src/test/scala/example/base/dao/PersonDaoSpec.scala
@@ -0,0 +1,28 @@
+package example.base.dao
+
+import java.time.{ZoneId, ZonedDateTime}
+import org.scalatest.{fixture, DiagrammedAssertions}
+import scalikejdbc.scalatest.AutoRollback
+import scalikejdbc.{applyUpdate, autoNamedValues, insert}
+import scalikejdbc.DBSession
+
+final class PersonDaoSpec
+    extends fixture.FlatSpec
+    with DiagrammedAssertions
+    with AutoRollback
+    with settings.DBSettings {
+
+  behavior of "PersonDao"
+
+  val zonedDateTime = ZonedDateTime.of(2020, 6, 8, 0, 0, 0, 0, ZoneId.systemDefault)
+
+  val persion = PersonEntity("manabu", 26, zonedDateTime)
+
+  override def fixture(implicit session: DBSession): Unit = {
+    applyUpdate(insert.into(PersonDao).namedValues(autoNamedValues(persion, PersonDao.column)))
+  }
+
+  "findList" should "return a list of person entity" in { implicit session =>
+    assert(PersonDao.findList() === Seq(persion))
+  }
+}
