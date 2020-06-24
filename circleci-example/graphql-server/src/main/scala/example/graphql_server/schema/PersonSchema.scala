5b80496687d201d340932b3362452b82ce1937ff
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-24 21:58:45 +0900

    Fix a bit

--- circleci-example/graphql-server/src/main/scala/example/graphql_server/schema/PersonSchema.scala
+++ circleci-example/graphql-server/src/main/scala/example/graphql_server/schema/PersonSchema.scala
@@ -1,44 +1,12 @@
 package example.graphql_server.schema
 
 import example.rest_api.entity.PersonEntity
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.util.Date
-import sangria.ast.StringValue
-import sangria.marshalling.DateSupport
 import sangria.schema._
-import sangria.validation.ValueCoercionViolation
-import scala.util.{Failure, Success, Try}
 
 trait PersonSchema {
 
-  case object ZonedDateTimeCoercionViolation
-      extends ValueCoercionViolation("ZonedDateTime value expected")
-
-  def parseZonedDateTime(s: String) =
-    Try(ZonedDateTime.parse(s)) match {
-      case Success(date) => Right(date)
-      case Failure(_) => Left(ZonedDateTimeCoercionViolation)
-    }
-
   @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  implicit val ZonedDateTimeType = ScalarType[ZonedDateTime](
-    "ZonedDateTime",
-    coerceOutput = (d, caps) =>
-      if (caps.contains(DateSupport)) Date.from(d.toInstant)
-      else DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'").format(d),
-    coerceUserInput = {
-      case s: String => parseZonedDateTime(s)
-      case _ => Left(ZonedDateTimeCoercionViolation)
-    },
-    coerceInput = {
-      case StringValue(s, _, _, _, _) => parseZonedDateTime(s)
-      case _ => Left(ZonedDateTimeCoercionViolation)
-    }
-  )
-
-  @SuppressWarnings(Array("org.wartremover.warts.Any"))
-  val PictureType = ObjectType(
+  val PersonType = ObjectType(
     "Person",
     "The person",
     fields[Unit, PersonEntity](
---
+++ circleci-example/graphql-server/src/main/scala/example/graphql_server/schema/package.scala
@@ -0,0 +1,39 @@
+package example.graphql_server
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import sangria.ast.StringValue
+import sangria.marshalling.DateSupport
+import sangria.schema._
+import sangria.validation.ValueCoercionViolation
+import scala.util.{Failure, Success, Try}
+
+package object schema {
+
+  case object ZonedDateTimeCoercionViolation
+      extends ValueCoercionViolation("ZonedDateTime value expected")
+
+  def parseZonedDateTime(s: String) =
+    Try(ZonedDateTime.parse(s)) match {
+      case Success(date) => Right(date)
+      case Failure(_) => Left(ZonedDateTimeCoercionViolation)
+    }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
+  implicit val ZonedDateTimeType = ScalarType[ZonedDateTime](
+    "ZonedDateTime",
+    coerceOutput = (d, caps) =>
+      if (caps.contains(DateSupport)) Date.from(d.toInstant)
+      else DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'").format(d),
+    coerceUserInput = {
+      case s: String => parseZonedDateTime(s)
+      case _ => Left(ZonedDateTimeCoercionViolation)
+    },
+    coerceInput = {
+      case StringValue(s, _, _, _, _) => parseZonedDateTime(s)
+      case _ => Left(ZonedDateTimeCoercionViolation)
+    }
+  )
+
+}
