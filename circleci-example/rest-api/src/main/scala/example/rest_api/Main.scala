d7e703b484849d806aa79a04575e760870e5b26c
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-24 11:59:29 +0900

    Add route directory

--- circleci-example/rest-api/src/main/scala/example/rest_api/Main.scala
+++ circleci-example/rest-api/src/main/scala/example/rest_api/Main.scala
@@ -1,68 +1,14 @@
 package example.rest_api
 
 import akka.http.scaladsl.server.{HttpApp, Route}
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.model.StatusCodes.OK
-import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import example.core.settings.DBSettings
-import example.rest_api.component.PersonComponent
-import example.rest_api.entity.PersonEntity
-import io.circe.syntax._
+import example.rest_api.route.PersonRoute
 
-import scalikejdbc.DB
-
-object Main extends HttpApp with App with PersonComponent with DBSettings {
+object Main extends HttpApp with App with DBSettings with PersonRoute {
 
   override def routes: Route =
     Route.seal {
-      // format: off
-      path("persons") {
-        get {
-          parameter("limit".as[Int], "offset".as[Int]) { (limit, offset) =>
-            DB.readOnly { implicit session =>
-              val list = personHandler.findList(limit, offset)
-
-              complete(
-                HttpEntity(ContentTypes.`application/json`, list.asJson.toString)
-              )
-            }
-          }
-        } ~
-        post {
-          entity(as[PersonEntity]) { person =>
-            DB.localTx { implicit session =>
-              val _ = personHandler.create(person)
-            }
-            complete(OK)
-          }
-        } ~
-        put {
-          entity(as[PersonEntity]) { person =>
-            DB.localTx { implicit session =>
-              val _ = personHandler.update(person)
-            }
-            complete(OK)
-          }
-        }
-      } ~
-      path("persons" / IntNumber) { id =>
-        get {
-          DB.readOnly { implicit session =>
-            val option = personHandler.findById(id)
-
-            complete(
-              HttpEntity(ContentTypes.`application/json`, option.asJson.toString)
-            )
-          }
-        } ~
-        delete {
-          DB.localTx { implicit session =>
-            val _ = personHandler.delete(id)
-          }
-          complete(OK)
-        }
-      }
-      // format: on
+      personRoutes
     }
 
   startServer(host = "localhost", port = 8181)
