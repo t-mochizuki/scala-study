b97e324ee16126f92dacaaadd89c8443ff8b3ba3
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-24 22:16:28 +0900

    Add repository directory

---
+++ circleci-example/graphql-server/src/main/scala/example/graphql_server/repository/PersonRepo.scala
@@ -0,0 +1,21 @@
+package example.graphql_server.repository
+
+import java.time.{ZonedDateTime, ZoneId}
+import example.rest_api.entity.PersonEntity
+
+class PersonRepo {
+
+  val zonedDateTime = ZonedDateTime.of(2020, 6, 8, 0, 0, 0, 0, ZoneId.systemDefault)
+
+  private val people = (
+    PersonEntity(1, "manabu", zonedDateTime) ::
+    PersonEntity(2, "keiko", zonedDateTime.minusDays(123)) ::
+    Nil
+  )
+
+  def person(id: Int): Option[PersonEntity] =
+    people.find(_.id == id)
+
+  def persons: List[PersonEntity] = people
+
+}
