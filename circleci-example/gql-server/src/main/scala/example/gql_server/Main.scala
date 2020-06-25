a5c47478e0c0fb26348ac2f1f4d608e5c67ab843
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-06-25 18:44:17 +0900

    To do close all connection pool, call DBs.closeAll

--- circleci-example/gql-server/src/main/scala/example/gql_server/Main.scala
+++ circleci-example/gql-server/src/main/scala/example/gql_server/Main.scala
@@ -1,10 +1,19 @@
 package example.gql_server
 
+import akka.Done
+import akka.actor.ActorSystem
 import akka.http.scaladsl.server.{HttpApp, Route}
 import example.core.settings.DBSettings
 import example.gql_server.routing.GraphQLRoute
+import scala.util.Try
+import scalikejdbc.config.DBs
 
 object Main extends HttpApp with App with DBSettings with GraphQLRoute {
+
+  override def postServerShutdown(attempt: Try[Done], system: ActorSystem): Unit = {
+    super.postServerShutdown(attempt, system)
+    DBs.closeAll()
+  }
 
   override def routes: Route =
     Route.seal {
--- circleci-example/rest-api/src/main/scala/example/rest_api/Main.scala
+++ circleci-example/rest-api/src/main/scala/example/rest_api/Main.scala
@@ -1,10 +1,19 @@
 package example.rest_api
 
+import akka.Done
+import akka.actor.ActorSystem
 import akka.http.scaladsl.server.{HttpApp, Route}
 import example.core.settings.DBSettings
 import example.rest_api.routing.PersonRoute
+import scala.util.Try
+import scalikejdbc.config.DBs
 
 object Main extends HttpApp with App with DBSettings with PersonRoute {
+
+  override def postServerShutdown(attempt: Try[Done], system: ActorSystem): Unit = {
+    super.postServerShutdown(attempt, system)
+    DBs.closeAll()
+  }
 
   override def routes: Route =
     Route.seal {
