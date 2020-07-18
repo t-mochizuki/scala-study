70974ba365dbb89140c7667dcaa4442ce1706b2e
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-07-18 15:07:19 +0900

    Update ScalaTest to 3.2.0
    cf. https://www.scalatest.org/release_notes/3.2.0

--- mockito-example/project/Dependencies.scala
+++ mockito-example/project/Dependencies.scala
@@ -2,11 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % Test
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.0" % Test
+  val scalatestFlatspec = "org.scalatest" %% "scalatest-flatspec" % "3.2.0" % Test
   val mockito = "org.mockito" % "mockito-core" % "3.4.0" % Test
 
   val rootDeps = Seq(
     scalatest,
+    scalatestFlatspec,
     mockito
   )
 
