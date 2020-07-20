6b127be8dec6162cc31cb768d25c88a7e9307354
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-07-20 12:16:26 +0900

    Add an authentication using cookies
    cf. https://github.com/softwaremill/akka-http-session-faq/tree/cde132dc2cd309656f7fd24bbb066c1b802b79f1#why-would-i-use-cookies
    cf. https://github.com/softwaremill/akka-http-session/blob/9b981851ad3ef9626ceb11b64528eba69e2ad27d/example/src/main/scala/com/softwaremill/example/session/MyScalaSession.scala

--- akka-http-session-example/project/Dependencies.scala
+++ akka-http-session-example/project/Dependencies.scala
@@ -5,17 +5,22 @@ object Dependencies {
   object Version {
     val akka = "2.6.6"
     val akkaHttp = "10.1.12"
+    val akkaHttpSession = "0.5.11"
   }
 
   val akkaHttp = "com.typesafe.akka" %% "akka-http" % Version.akkaHttp
   val akkaHttpTestkit = "com.typesafe.akka" %% "akka-http-testkit" % Version.akkaHttp % Test
+  val akkaHttpSession = "com.softwaremill.akka-http-session" %% "core" % Version.akkaHttpSession
   val akkaStream = "com.typesafe.akka" %% "akka-stream" % Version.akka
   val akkaStreamTestKit = "com.typesafe.akka" %% "akka-stream-testkit" % Version.akka % Test
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % Test
 
   val serverDeps = Seq(
     akkaHttp,
+    akkaHttpTestkit,
+    akkaHttpSession,
     akkaStream,
+    akkaStreamTestKit,
     scalatest)
 
   val rootDeps = Nil
--- akka-http-session-example/src/main/scala/example/Main.scala
+++ akka-http-session-example/src/main/scala/example/Main.scala
@@ -1,14 +1,45 @@
 package example
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{HttpApp, Route}
+import com.softwaremill.session.SessionDirectives.{setSession, invalidateSession, requiredSession}
+import com.softwaremill.session.SessionOptions.{oneOff, usingCookies}
+import com.softwaremill.session.{SessionConfig, SessionManager, SessionUtil}
 
 object Main extends HttpApp with App {
 
+  val sessionConfig = SessionConfig.default(SessionUtil.randomServerSecret())
+
+  implicit val sessionManager = new SessionManager[Session](sessionConfig)
+
   override def routes: Route =
     Route.seal {
+      path("login") {
+        post {
+          formFields("id", "password"){ case (id, password) =>
+            if(id == "admin" && password == "admin"){
+              setSession(oneOff, usingCookies, Session(id)) {
+                complete(StatusCodes.OK)
+              }
+            } else {
+              complete(StatusCodes.Unauthorized)
+            }
+          }
+        }
+      } ~
+      path("logout") {
+        post {
+          invalidateSession(oneOff, usingCookies){
+            complete(StatusCodes.OK)
+          }
+        }
+      } ~
       path("hello") {
         get {
-          complete("Hello, world")
+          requiredSession(oneOff, usingCookies) { session =>
+            println(session.id)
+            complete("Hello, world")
+          }
         }
       }
     }
