1217771da9a645ee326259e66b246117414eddde
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-07-28 17:09:13 +0900

    To re-create a session, use refreshable instead of oneOff
    cf. https://github.com/softwaremill/akka-http-session#refresh-tokens-aka-remember-me

--- akka-http-session-example/src/main/scala/example/Main.scala
+++ akka-http-session-example/src/main/scala/example/Main.scala
@@ -2,9 +2,12 @@ package example
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.{HttpApp, Route}
-import com.softwaremill.session.SessionDirectives.{setSession, invalidateSession, requiredSession}
-import com.softwaremill.session.SessionOptions.{oneOff, usingCookies}
-import com.softwaremill.session.{SessionConfig, SessionManager}
+import com.softwaremill.session.SessionDirectives.{invalidateSession, requiredSession, setSession}
+import com.softwaremill.session.SessionOptions.{refreshable, usingCookies}
+import com.softwaremill.session.{InMemoryRefreshTokenStorage, SessionConfig, SessionManager}
+import com.softwaremill.session.RefreshTokenStorage
+
+import scala.concurrent.ExecutionContext
 
 object Main extends HttpApp with App {
 
@@ -12,39 +15,53 @@ object Main extends HttpApp with App {
 
   implicit val sessionManager = new SessionManager[Session](sessionConfig)
 
-  def createRoutes()(implicit sessionManager: SessionManager[Session]): Route =
+  implicit val refreshTokenStorage = new InMemoryRefreshTokenStorage[Session] {
+    def log(msg: String) = println(msg)
+  }
+
+  def createRoutes()(implicit
+      sessionManager: SessionManager[Session],
+      refreshTokenStorage: RefreshTokenStorage[Session],
+      ec: ExecutionContext
+  ): Route =
+    // format: off
     path("login") {
       post {
-        formFields("id", "password") { case (id, password) =>
-          if (id == "admin" && password == "admin") {
-            setSession(oneOff, usingCookies, Session(id)) {
-              complete(StatusCodes.OK)
+        formFields("id", "password") {
+          case (id, password) =>
+            if (id == "admin" && password == "admin") {
+              setSession(refreshable, usingCookies, Session(id)) {
+                complete(StatusCodes.OK)
+              }
+            } else {
+              complete(StatusCodes.Unauthorized)
             }
-          } else {
-            complete(StatusCodes.Unauthorized)
-          }
         }
       }
     } ~
     path("logout") {
       post {
-        invalidateSession(oneOff, usingCookies) {
+        invalidateSession(refreshable, usingCookies) {
           complete(StatusCodes.OK)
         }
       }
     } ~
     path("hello") {
       get {
-        requiredSession(oneOff, usingCookies) { session =>
+        requiredSession(refreshable, usingCookies) { session =>
           complete(s"Hello, ${session.id}")
         }
       }
     }
+    // format: on
 
-  override def routes: Route =
+  override def routes: Route = {
+    implicit val ec = systemReference.get.dispatcher
+
     Route.seal {
       createRoutes()
     }
+  }
 
   startServer(host = "0.0.0.0", port = 3000)
 
