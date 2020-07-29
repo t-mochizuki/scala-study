f57a9efbefb037eee341eedbc1c07e189a131649
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-07-29 11:21:41 +0900

    To store an instance on Redis, serialize the instance using circe

--- scala-redis-example/project/Dependencies.scala
+++ scala-redis-example/project/Dependencies.scala
@@ -2,14 +2,23 @@ import sbt._
 
 object Dependencies {
 
-  object Version {}
+  object Version {
+    val circe = "0.13.0"
+  }
 
   val redisclient = "net.debasishg" %% "redisclient" % "3.30"
+
+  val circeCore = "io.circe" %% "circe-core" % Version.circe
+  val circeGeneric = "io.circe" %% "circe-generic" % Version.circe
+  val circeParser = "io.circe" %% "circe-parser" % Version.circe
 
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % Test
 
   val rootDeps = Seq(
     redisclient,
+    circeCore,
+    circeGeneric,
+    circeParser,
     scalatest
   )
 
--- scala-redis-example/src/main/scala/example/Store.scala
+++ scala-redis-example/src/main/scala/example/Store.scala
@@ -1,3 +1,11 @@
 package example
 
-case class Store(session: Session)
+import io.circe.generic.semiauto._
+import io.circe.{Decoder, Encoder}
+
+final case class Store(session: Session)
+
+object Store {
+  implicit val encoder: Encoder[Store] = deriveEncoder
+  implicit val decoder: Decoder[Store] = deriveDecoder
+}
