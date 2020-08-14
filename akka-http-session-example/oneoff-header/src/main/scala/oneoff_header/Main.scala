a6e800a48ab21533ab841551c7e8a2814b99fea6
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-08-14 14:45:01 +0900

    Get id by decoding a value of Authorization header

--- akka-http-session-example/oneoff-header/src/main/scala/oneoff_header/Main.scala
+++ akka-http-session-example/oneoff-header/src/main/scala/oneoff_header/Main.scala
@@ -1,10 +1,10 @@
 package oneoff_header
 
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{HttpHeader, StatusCodes}
 import akka.http.scaladsl.server.{HttpApp, Route}
 import com.softwaremill.session.SessionDirectives.{invalidateSession, requiredSession, setSession}
 import com.softwaremill.session.SessionOptions.{oneOff, usingHeaders}
-import com.softwaremill.session.{SessionConfig, SessionManager}
+import com.softwaremill.session.{SessionConfig, SessionManager, SessionResult}
 
 object Main extends HttpApp with App {
 
@@ -39,8 +39,21 @@ object Main extends HttpApp with App {
     } ~
     path("hello") {
       get {
-        requiredSession(oneOff, usingHeaders) { session =>
-          complete(s"Hello, ${session.id}")
+        val headerName = sessionManager.config.sessionHeaderConfig.getFromClientHeaderName // Authorization
+        val lowerCaseName = headerName.toLowerCase
+        extractRequest { httpRequest =>
+          val v = httpRequest.headers.collectFirst {
+            case HttpHeader(`lowerCaseName`, value) => value
+          }.getOrElse("")
+
+          val id = sessionManager.clientSessionManager.decode(v) match {
+            case s: SessionResult.Decoded[Session] => s.session.id
+            case _ => ""
+          }
+
+          requiredSession(oneOff, usingHeaders) { session =>
+            complete(s"$id == ${session.id}")
+          }
         }
       }
     }
