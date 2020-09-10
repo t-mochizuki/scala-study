84a41a8a8c930ca3dbc7c48e958e16fe1b7db5f3
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2020-09-10 10:43:58 +0900

    fix a bit
    cf. https://github.com/yahoojp-marketing/ads-display-api-documents/tree/2a740672ea52022ef38abf50e6ae2acd147feb61/design/v2/campaign

--- yahoo/ads-display-api/api-client/src/main/scala/api_client/Campaign.scala
+++ yahoo/ads-display-api/api-client/src/main/scala/api_client/Campaign.scala
@@ -3,7 +3,7 @@ package api_client
 import io.circe.generic.semiauto._
 import io.circe.{Decoder, Encoder}
 
-case class Campaign(accountId: Long, campaignId: Long, campaignName: String)
+case class Campaign(accountId: Option[Long], campaignId: Option[Long], campaignName: Option[String])
 
 object Campaign {
   implicit val encoder: Encoder[Campaign] = deriveEncoder
--- yahoo/ads-display-api/api-client/src/main/scala/api_client/CampaignServicePage.scala
+++ yahoo/ads-display-api/api-client/src/main/scala/api_client/CampaignServicePage.scala
@@ -3,7 +3,7 @@ package api_client
 import io.circe.generic.semiauto._
 import io.circe.{Decoder, Encoder}
 
-case class CampaignServicePage(totalNumEntries: Int, values: Option[Seq[CampaignServiceValue]])
+case class CampaignServicePage(totalNumEntries: Int, values: Option[Seq[Option[CampaignServiceValue]]])
 
 object CampaignServicePage {
   implicit val encoder: Encoder[CampaignServicePage] = deriveEncoder
